aeb40a1c8238f75a87ddc309e457e115507fdb15
michellemej22596 <mej22596@uvg.edu.gt>
2024-07-24 21:29:59 -0600

    Second polygon done

---
+++ polygon2/target/debug/deps/autocfg-e5d2ce40cb085c0f.d
@@ -0,0 +1,10 @@
+C:\Users\usuario\Desktop\Graficas\Lab\Lab1-FillingPolygon\polygon2\target\debug\deps\libautocfg-e5d2ce40cb085c0f.rmeta: C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs
+
+C:\Users\usuario\Desktop\Graficas\Lab\Lab1-FillingPolygon\polygon2\target\debug\deps\libautocfg-e5d2ce40cb085c0f.rlib: C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs
+
+C:\Users\usuario\Desktop\Graficas\Lab\Lab1-FillingPolygon\polygon2\target\debug\deps\autocfg-e5d2ce40cb085c0f.d: C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs
+
+C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\lib.rs:
+C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\error.rs:
+C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\rustc.rs:
+C:\Users\usuario\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.3.0\src\version.rs:
